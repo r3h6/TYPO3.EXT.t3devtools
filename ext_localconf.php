06d78cd38654d6857f5d9eb8cfe95ac2d6ffd769
r3h6 <r3h6@outlook.com>
2020-03-15 22:34:08 +0100

    [CLEANUP] Clean ext_* files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,9 @@
 <?php
+defined('TYPO3_MODE') || die('Access denied.');
 
-/** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $dispatcher */
-// $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-//     TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class
-// );
-// $dispatcher->connect(
-//     \TYPO3\CMS\Core\Resource\ResourceFactory::class,
-//     \TYPO3\CMS\Core\Resource\ResourceFactoryInterface::SIGNAL_PostProcessStorage,
-//     \R3H6\T3devtools\Resource\Slot\ResourceFactorySlot::class,
-//     'initializeResourceStorage'
-// );
+call_user_func(
+    function()
+    {
+
+    }
+);
